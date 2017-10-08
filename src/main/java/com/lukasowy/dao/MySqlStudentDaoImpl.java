31eedd77d59c1c555a99909be9c464f78e88946a
lukasowy <lukasowy@gmail.com>
2017-10-08 20:15:16 +0200

    Added comments

--- src/main/java/com/lukasowy/dao/MySqlStudentDaoImpl.java
+++ src/main/java/com/lukasowy/dao/MySqlStudentDaoImpl.java
@@ -17,6 +17,7 @@ public class MySqlStudentDaoImpl implements StudentDao{
     @Autowired
     private JdbcTemplate jdbcTemplate;
 
+    //It make it easy to create a student instance in database
     public static class StudentRowMapper implements RowMapper<Student>{
         @Override
         public Student mapRow(ResultSet resultSet, int i) throws SQLException {
@@ -28,6 +29,7 @@ public class MySqlStudentDaoImpl implements StudentDao{
         }
     }
 
+    //GET request
     @Override
     public Collection<Student> getAllStudent() {
         final String sql = "SELECT id, name, course FROM students";
@@ -35,19 +37,21 @@ public class MySqlStudentDaoImpl implements StudentDao{
         return students;
     }
 
+    //GET request by Id
     @Override
     public Student getStudentById(int id) {
         final String sql = "SELECT id, name, course FROM students where id = ?";
         Student student = jdbcTemplate.queryForObject(sql, new StudentRowMapper(), id);
         return student;
     }
-
+    //DELETE request
     @Override
     public void removeStudentById(int id) {
         final String sql = "DELETE FROM students WHERE id= ?";
         jdbcTemplate.update(sql, id);
     }
 
+    //POST request
     @Override
     public void updateStudent(Student student) {
         final String sql = "UPDATE students SET name = ?, course = ? WHERE id = ?";
@@ -57,6 +61,7 @@ public class MySqlStudentDaoImpl implements StudentDao{
         jdbcTemplate.update(sql, new Object[] {name, course, id});
     }
 
+    //PUT request
     @Override
     public void insertStudent(Student student) {
         final String sql = " INSERT INTO students (name, course) VALUES (?, ?)";
